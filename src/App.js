da0a16950d017993d01b6f6a110ed9b77772fc31
annguyez <congannguyenviet@gmail.com>
2020-12-04 00:24:57 +0700

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import 'firebase/auth';
+import withFirebaseAuth from 'react-with-firebase-auth'
+import firebase from 'firebase';
+import 'firebase/auth';
+import firebaseConfig from './firebaseConfig';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const firebaseAppAuth = firebaseApp.auth();
+  const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+  
+};
+const {
+  user,
+  signOut,
+  signInWithGoogle,
+} = this.props;
+return (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      {
+        user 
+          ? <p>Hello, {user.displayName}</p>
+          : <p>Please sign in.</p>
+      }
+      {
+        user
+          ? <button onClick={signOut}>Sign out</button>
+          : <button onClick={signInWithGoogle}>Sign in with Google</button>
+      }
+    </header>
+  </div>
+);
 }
 
-export default App;
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(App);
---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyBKVmS3BOLaXWioiilA5tOPz-bI8v5twOw",
+    authDomain: "reactjs-login-eed6e.firebaseapp.com",
+    projectId: "reactjs-login-eed6e",
+    storageBucket: "reactjs-login-eed6e.appspot.com",
+    messagingSenderId: "146343098451",
+    appId: "1:146343098451:web:437636e70516cff6202ea6",
+    measurementId: "G-KJ3TDMMVNP"
+  };
+  export default config;
